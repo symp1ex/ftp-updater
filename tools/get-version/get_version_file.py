cfcfcbe3ea381384688d77183fe6ac7fd0f4262c
Eugene <symplex@live.com>
2024-10-27 04:18:39 +0300

    Add files via upload

---
+++ tools/get-version/get_version_file.py
@@ -0,0 +1,12 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="version.txt",
+    version="1.1.3.0",
+    #company_name="My Imaginary Company",
+    #file_description="Random Text Generator",
+    #internal_name="Simple App",
+    #legal_copyright="© My Imaginary Company. All rights reserved.",
+    original_filename="getad.exe",
+    product_name="getad"
+)
